3511082d1e41a94c5c66f447889e5aa85bcc2fd4
Noah Warnke <noah.warnke@gmail.com>
2018-10-02 19:49:21 -0400

    Add a readme and some comments to code.js.

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Interweave.network
+
+An Ethereum dapp where users explore and create a collaborative network of imaginary places and paths between them. Currently in the early stages of design and development.
--- src/code.js
+++ src/code.js
@@ -7,22 +7,62 @@ class Web3Handler {
     this.localWeb3 = undefined;
   }
   
+  /** @returns the injected instance of the web3 interface, if it exists. */
   web3Interface() {
     return window.web3;
   }
   
+  /** @returns true if injected instance of web3 interface doesn't exist, or false if it does. */
   web3InterfaceUnavailable() {
     return (typeof this.web3Interface() === 'undefined');
   }
   
+  /** @returns the included Web3 library object, if it exists. */
   web3Library() {
     return window.Web3;
   }
   
+  /** @returns true if Web3 library object doesn't exist, or false if it does. */
   web3LibraryUnavailable() {
     return (typeof this.web3Library() === 'undefined');
   }
   
+  /** @returns the default Eth account from the injected web3 interface, if it exists. */
+  getDefaultAccount() {
+    if (this.web3InterfaceUnavailable()) {
+      console.log("No injected web3 interface, cannot get default account.")
+      return;
+    }
+    return this.web3Interface().eth.defaultAccount;
+  }
+  
+  /** @returns a string indicating the current network, mapped from the injected web3 interface, if it exists. */
+  getNetwork() {
+    if (this.web3InterfaceUnavailable()) {
+      console.log("No injected web3 interface, cannot get current network.")
+      return;
+    }
+    return new Promise((resolve, reject) => {
+      this.web3Interface().version.getNetwork((err, netId) => {
+        if (err) {
+          reject(err);
+        }
+        else {
+          let networks = ["Unknown", "Mainnet", "Morden", "Ropsten", "Rinkeby", "Kovan"];
+          resolve(
+            netId < networks.length
+              ? networks[netId]
+              : "Unknown"
+          );
+        }
+      });
+    });
+  }
+  
+  /**
+   * Sets up state variables for this Web3Hander, including getting the injected provider
+   * and creating a new Web3 instance from it.
+   */
   async setUpProvider() {
     console.log("Window loaded...");
     
@@ -64,36 +104,11 @@ class Web3Handler {
     document.querySelector('#value').innerHTML = value;
   }
   
-  getDefaultAccount() {
-    if (this.web3InterfaceUnavailable()) {
-      console.log("No injected web3 interface, cannot get default account.")
-      return;
-    }
-    return this.web3Interface().eth.defaultAccount;
-  }
-  
-  getNetwork() {
-    if (this.web3InterfaceUnavailable()) {
-      console.log("No injected web3 interface, cannot get current network.")
-      return;
-    }
-    return new Promise((resolve, reject) => {
-      this.web3Interface().version.getNetwork((err, netId) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          let networks = ["Unknown", "Mainnet", "Morden", "Ropsten", "Rinkeby", "Kovan"];
-          resolve(
-            netId < networks.length
-              ? networks[netId]
-              : "Unknown"
-          );
-        }
-      });
-    });
-  }
-  
+  /**
+   * Call the getLink function on the test contract.
+   * @param ethAddress the Eth address to look up in the contract.
+   * @returns the string the Eth address maps to in the contract (may be false).
+   */
   testContractGetValue(ethAddress) {
     if (this.web3LibraryUnavailable() || this.web3InterfaceUnavailable()) {
       console.log("Dapp not ready, cannot try calling a contract.");
@@ -108,7 +123,13 @@ class Web3Handler {
     return methods.getLink(ethAddress).call();
   }
   
-  testContractSetValue(account, newValue) {
+  /**
+   * Send a new transaction calling the setLink function on the test contract.
+   * @param ethAddress the Eth address to set the value for in the contract.
+   * @param newValue the new string (must be <= 32 characters) to set the
+   * @returns the string the Eth address maps to in the contract (may be false).
+   */
+  testContractSetValue(ethAddress, newValue) {
     if (newValue.length > 32) {
       console.log("Too long.");
       return;
@@ -125,14 +146,13 @@ class Web3Handler {
     var methods = tokenContract.methods;
     
     return new Promise((resolve, reject) => {
-      methods.setLink(newValue).send({from: account, value: 0})
+      methods.setLink(newValue).send({from: ethAddress, value: 0})
         .on("receipt", (receipt) => resolve(receipt))
         .on("error", (error) => reject(error))
       ;
     });
   }
 }
-
 
 // Instantiate my handler.
 let myWeb3Handler = new Web3Handler();
