c0b811eeb15986cb7b81b19938ba6c7455208631
Noah Warnke <noah.warnke@gmail.com>
2018-11-03 00:44:38 -0400

    Modify non-rendered Node appearance a bit.

--- free/components/TheRenderArea.js
+++ free/components/TheRenderArea.js
@@ -2,9 +2,11 @@ export default {
   template: `
     <div id="render">
       <div id="norender-info" v-if="node.ipfs !== undefined">
-        <p>Current node key: {{node.key}}</p>
-        <p>Current node IPFS: <a target="_blank" v-bind:href="'https://ipfs.io/ipfs/' + node.ipfs">{{node.ipfs}}</a></p>
-        <textarea class="node-file-text">{{node.data}}</textarea>
+        <div class="node-key-and-ipfs">
+          <p>Current node key: {{node.key}}</p>
+          <p>Current node IPFS: <a target="_blank" v-bind:href="'https://ipfs.io/ipfs/' + node.ipfs">{{node.ipfs}}</a></p>
+        </div>
+        <textarea class="node-file-text" readonly="readonly">{{node.data}}</textarea>
       </div>
     </div>
   `,
