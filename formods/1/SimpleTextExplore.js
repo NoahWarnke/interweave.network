c0a86706b38a9ce69f9e983bb4c0147dcfcebe26
Noah Warnke <noah.warnke@gmail.com>
2018-11-05 21:29:58 -0500

    Add new verbs list to SimpleText in prep for new format, and also update interweavegrotto example json to that format.

--- formods/1/SimpleTextExplore.js
+++ formods/1/SimpleTextExplore.js
@@ -21,6 +21,45 @@ export default {
   },
   data: function() {
     return {
+      verbs: {
+        "0": ["examine", "look", "look at", "inspect"],
+        "1": ["listen", "listen to"],
+        "2": ["smell", "sniff", "sniff at"],
+        "3": ["taste", "lick"],
+        "4": ["touch", "feel"],
+        "5": ["go", "go to", "travel", "travel to"],
+        "6": ["go into", "enter"],
+        "7": ["go onto", "climb onto"],
+        "8": ["follow", "walk along", "walk down"],
+        "9": ["swim", "swim across", "swim into", "wade", "wade across", "wade into"],
+        "10": ["climb", "climb up", "climb onto", "climb over"]
+      },
+      verbFailNoTarget: {
+        "0": "You can't see anything like that here.",
+        "1": "You don't hear anything like that here.",
+        "2": "There isn't anything like that here to smell.",
+        "3": "There isn't anything like that here to taste.",
+        "4": "There isn't anything like that here to touch.",
+        "5": "There isn't anywhere like that here that you could go.",
+        "6": "There isn't anything like that here that you could go into.",
+        "7": "There isn't anything like that here that you could go onto.",
+        "8": "There isn't anything like that here that you could follow.",
+        "9": "You don't see anything like that here that you could swim into.",
+        "10": "You don't see anything like that here that you could climb."
+      },
+      verbFailTarget: {
+        "0": "You can't make out any more detail.",
+        "1": "There is nothing more to be heard.",
+        "2": "You can't smell that.",
+        "3": "You mustn't lick that!",
+        "4": "That seems impractical or uninformative.",
+        "5": "You can't go there.",
+        "6": "You can't enter that.",
+        "7": "You can't get onto that.",
+        "8": "You can't follow that.",
+        "9": "That is not somewhere you could swim!",
+        "10": "You can't climb that!"
+      },
       consoleText:"",
       consoleInput: "",
       edgeStart: false,
