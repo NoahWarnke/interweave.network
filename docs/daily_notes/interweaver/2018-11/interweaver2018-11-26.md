163fc2bc180700270cf5d0738a92f570afa1873c
Noah Warnke <noah.warnke@gmail.com>
2018-11-26 21:06:20 -0500

    Add some doc comments to Node class.

---
+++ docs/daily_notes/interweaver/2018-11/interweaver2018-11-26.md
@@ -0,0 +1,55 @@
+# Interweaver's to-do items, 2018-11-26:
+
+- [ ] Build Version 1 (the "free version", i.e. without any way to exchange money) of the Interweave Network.
+  - [ ] Create functional DApp:
+    - [ ] Make build mode:
+      - [ ] Add 'draft' Nodes
+        - [ ] Make SimpleTextBuild work.
+          - [ ] Make all the properties addable/removable/editable.
+        - [ ] Give Node a method to export JSON version of the contained iData (plus name/format/formatVersion)
+        - [ ] Figure out how to interface with the IPFS API and actually accomplish that, getting back the IPFS hash.
+        - [ ] Make a button to deploy to blockchain.
+          - [ ] Actually add the new Node
+          - [ ] move the Node data from draftIpfsData to ipfsData,
+          - [ ] remove the fake key from myDraftNodeKeys,
+          - [ ] add real one to myNodeKeys
+          - [ ] add blockchain data to nodes
+          - [ ] Hunt down all instances of the fake node key in myDraftEdgeProposalKeys and replace with the real one.
+      - [ ] My Edge Proposals
+        - [ ] Create an app mode for myedgeproposals
+        - [ ] Create a test EdgeProposal or two.
+        - [ ] Show paged view of all my EdgeProposal keys.
+        - [ ] Load EdgeProposal blockchain data (so getting the two Node keys and epmess and slots).
+      - [ ] Edge Proposal
+        - [ ] Create an App mode for edgeproposal
+        - [ ] Show current EdgeProposal key
+        - [ ] Show blockchain data
+        - [ ] Load Node data and show the names
+        - [ ] Make clicking on the Nodes go to them.
+    - [ ] Extensively test!
+    - [ ] Share somewhere, and fix bugs that people find, haha. This being the "free version", less (but not nothing) is at stake.
+  - [ ] Smart contracts
+    - [ ] When DApp is ready to share, compile and deploy to testnet!
+    - [ ] When DApp is tested and ready to go, deploy to mainnet!
+    
+- [ ] Build Version 2 (the "payable version", i.e. with the ability to pay/be paid for EdgeProposals, and Nodes are ERC-721s)
+
+- [ ] Write whitepaper:
+    - [ ] Fill in sections. Note: leave out implementation details (anything as specific as which smart contracts or fields need to be created.) That goes in yellow paper.
+      - [ ] Rationale for Interweave Network
+      - [ ] Philosophy
+      - [ ] Architecture
+      - [ ] Applications
+      - [ ] Challenges
+      - [ ] Summary
+      - [ ] Further Reading
+    - [ ] Let sit for a few days while doing other things.
+    - [ ] Revisions, round 1.
+    - [ ] Let sit for another few days while doing other things.
+    - [ ] Revisions, round 2.
+    - [ ] Publish (reddit? medium? Lol, I've seen enough whitepapers to know where this is going. Not that I have any reach...)
+ 
+
+
+
+ 
--- free/js/Node.js
+++ free/js/Node.js
@@ -2,6 +2,12 @@
 /** Represents all the data we have on a given Node. */
 export default class Node {
   
+  /**
+   * Constructor for a new Node item with the given key and type.
+   * Sets other properties to initial values.
+   * @param key The new Node's key string.
+   * @param type The new Node's type string (either 'deployed' or 'draft').
+   */
   constructor(key, type) {
     
     this._key = key;
@@ -21,55 +27,100 @@ export default class Node {
     this._formatVersion = 1;
   }
   
+  /**
+   * @returns This Node's key.
+   */
   get key() {
     return this._key;
   }
   
+  /**
+   * @returns This Node's type (either 'deployed' or 'draft')
+   */
   get type() {
     return this._type;
   }
   
+  /**
+   * @returns This Node's blockchain data object.
+   */
   get bData() {
     return this._bData;
   }
   
+  /**
+   * @returns This Node's blockchain data status: 'init', 'pending', 'failed', or 'successful'.
+   */
   get bStatus() {
     return this._bStatus;
   }
   
+  /**
+   * @returns This Node's blockchain error message, or undefined if none.
+   */
   get bError() {
     return this._bError;
   }
   
+  /**
+   * @returns This Node's IPFS data object.
+   */
   get iData() {
     return this._iData;
   }
   
+  /**
+   * @returns This Node's IPFS data status: 'init', 'pending', 'failed', or 'successful'.
+   */
   get iStatus() {
     return this._iStatus;
   }
   
+  /**
+   * @returns This Node's IPFS error message, or undefined if none.
+   */
   get iError() {
     return this._iError;
   }
   
-  setKey(key, keyType) {
+  /**
+   * Set this Node's key and type.
+   * @param key The Node's new key.
+   * @param type The Node's new type, 'deployed' or 'draft'
+   */
+  setKey(key, type) {
     this._key = key;
     this._type = type;
   }
   
+  /**
+   * Set this Node's blockchain state.
+   * @param status The Node's new blockchain status, 'init', 'pending', 'failed', or 'successful'
+   * @param data The Node's new blockchain data, or undefined if none.
+   * @param error The Node's new blockchain error message, or undefined if none.
+   */
   setBlockchainState(status, data, error) {
     this._bStatus = status;
     this._bData = data;
     this._bError = error;
   }
   
+  /**
+   * Set this Node's IPFS state.
+   * @param status The Node's new IPFS status, 'init', 'pending', 'failed', or 'successful'
+   * @param data The Node's new IPFS data, or undefined if none.
+   * @param error The Node's new IPFS error message, or undefined if none.
+   */
   setIPFSState(status, data, error) {
     this._iStatus = status;
     this._iData = data;
     this._iError = error;
   }
   
+  /**
+   * Set this Node's name.
+   * @param name The Node's new name string.
+   */
   set name(name) {
     if (typeof name !== "string" || name.length < 1 || name.length > 32) {
       //throw new Error("Node name must be a non-empty string with <= 32 characters.");
@@ -78,10 +129,17 @@ export default class Node {
     this._name = name;
   }
   
+  /**
+   * @returns This Node's name.
+   */
   get name() {
     return this._name;
   }
   
+  /**
+   * Set this Node's format.
+   * @param format The Node's new format string.
+   */
   set format(format) {
     if (typeof format !== "string" || format.length === 0) {
       throw new Error("Node format must be a non-empty string.");
@@ -90,10 +148,17 @@ export default class Node {
     this._format = format;
   }
   
+  /**
+   * @returns this Node's format.
+   */
   get format() {
     return this._format;
   }
   
+  /**
+   * Set this Node's format version.
+   * @param formatVersion The Node's new format version integer.
+   */
   set formatVersion(formatVersion) {
     if (formatVersion !== parseInt(formatVersion)) {
       throw new Error("Node format version must be an integer.");
@@ -101,6 +166,9 @@ export default class Node {
     this._formatVersion = formatVersion;
   }
   
+  /**
+   * @returns This Node's format version.
+   */
   get formatVersion() {
     return this._formatVersion;
   }
