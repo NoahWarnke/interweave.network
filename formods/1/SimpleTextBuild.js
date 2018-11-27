618fc0fc69848e806c7ce7a1de88669263eff97c
Noah Warnke <noah.warnke@gmail.com>
2018-11-26 21:40:46 -0500

    Add edges to the SimpleTextBuild component.

--- formods/1/SimpleTextBuild.js
+++ formods/1/SimpleTextBuild.js
@@ -6,6 +6,7 @@ export default {
     <div id="simple-text-build">
       <div>
         <button v-on:click="view = 'description'">Description</button>
+        <button v-on:click="view = 'edges'">Edges</button>
         <button v-on:click="view = 'targets'">Target Sets</button>
         <button v-on:click="view = 'results'">Results</button>
         <button v-on:click="view = 'bindings'">Bindings</button>
@@ -14,6 +15,16 @@ export default {
         <p>Enter a short description for the Node. Explorers will see it when they first arrive.</p>
         <textarea v-model="content.shortDesc"></textarea>
       </div>
+      <div v-if="view === 'edges'">
+        <p>Edit the incoming/outgoing text for edges, and add or remove them.</p>
+        <ul>
+          <li v-for="(edge, index) of content.edges">
+            <span class="tag edge-tag">edge{{index}}</span>:
+            <span class="tag edge-tag">{{shorten(edge.enterDesc)}}</span>
+            <span class="tag edge-tag">{{shorten(edge.leaveDesc)}}</span>
+          </li>
+        </ul>
+      </div>
       <div v-if="view === 'targets'">
         <p>
           Edit the target sets. Each of these is a set of names that are treated as one possible 'target' for an explorer to apply a verb to.
@@ -21,6 +32,7 @@ export default {
         <ul>
           <li v-for="target of content.targets">
             <span class="tag target-tag" v-for="name of target" v-html="name === '' ? '&nbsp;' : name"></span>
+            <hr>
           </li>
         </ul>
       </div>
@@ -30,7 +42,7 @@ export default {
         </p>
         <ul>
           <li v-for="result of content.results">
-            <span class="tag result-tag">{{shortenResult(result)}}</span>
+            <span class="tag" v-bind:class="resultClass(result)">{{shorten(result)}}</span>
           </li>
         </ul>
       </div>
@@ -46,7 +58,7 @@ export default {
             <div v-for="(resultKey, targetKey) of binding">
               <span class="tag verb-tag">{{bindingKey}}</span>
               <span class="tag target-tag" v-html="firstNonEmptyTarget(content.targets[targetKey])"></span> =>
-              <span class="tag result-tag">{{shortenResult(content.results[resultKey])}}</span>
+              <span class="tag" v-bind:class="resultClass(content.results[resultKey])">{{shorten(content.results[resultKey])}}</span>
             </div>
             <hr>
           </li>
@@ -75,7 +87,7 @@ export default {
     }
   },
   methods: {
-    shortenResult: function(result) {
+    shorten: function(result) {
       return (result.length < 32
         ? result
         : result.substr(0, 32) + "..."
@@ -91,6 +103,12 @@ export default {
       }
       console.log("result: " + target[key]);
       return "&nbsp;";
+    },
+    resultClass: function(result) {
+      if (result.indexOf("edge") === 0 && result.length === 5) {
+        return 'edge-tag';
+      }
+      return 'result-tag';
     },
     addResult: function() {
       content.result
